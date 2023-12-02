6dc21a401e97723eb3aa2d78faef1480fd42f192
Meg Roth <megroth2@gmail.com>
2023-12-01 21:07:27 -0700

    refactor tests to use before (:each) do

--- spec/features/lifts/index_spec.rb
+++ spec/features/lifts/index_spec.rb
@@ -2,6 +2,23 @@ require "rails_helper"
 
 RSpec.describe "lifts index page", type: :feature do
 
+  before (:each) do 
+    @resort_2 = Resort.create!(name: "Copper Mountain",
+                              location: "Summit County, Colorado",
+                              peak_elevation: 12313,
+                              is_ikon_resort: true)
+    @lift_3 = Lift.create!(name:        "American Eagle",
+                          seats:       6,
+                          is_gondola:  true,
+                          id:          3,
+                          resort_id:   @resort_2.id)
+    @lift_4 = Lift.create!(name:        "American Flyer",
+                          seats:       6,
+                          is_gondola:  false,
+                          id:          4,
+                          resort_id:   @resort_2.id)
+  end
+
   it "has link to Resort Index" do
     visit "/lifts"
     
@@ -29,34 +46,19 @@ RSpec.describe "lifts index page", type: :feature do
   end
 
   it "can see all lifts and lift attributes" do
-    resort_2 = Resort.create!(name: "Copper Mountain",
-                              location: "Summit County, Colorado",
-                              peak_elevation: 12313,
-                              is_ikon_resort: true)
-    lift_3 = Lift.create!(name:        "American Eagle",
-                          seats:       6,
-                          is_gondola:  true,
-                          id:          3,
-                          resort_id:   resort_2.id)
-    lift_4 = Lift.create!(name:        "American Flyer",
-                          seats:       6,
-                          is_gondola:  false,
-                          id:          4,
-                          resort_id:   resort_2.id)
-
     visit "/lifts"
 
-    expect(page).to have_content(lift_3.name)
-    expect(page).to have_content(lift_3.seats)
-    expect(page).to have_content("Is Gondola: #{lift_3.is_gondola}")
-    expect(page).to have_content("Lift Id: #{lift_3.id}")
-    expect(page).to have_content("Resort Id: #{lift_3.resort_id}")
+    expect(page).to have_content(@lift_3.name)
+    expect(page).to have_content(@lift_3.seats)
+    expect(page).to have_content("Is Gondola: #{@lift_3.is_gondola}")
+    expect(page).to have_content("Lift Id: #{@lift_3.id}")
+    expect(page).to have_content("Resort Id: #{@lift_3.resort_id}")
 
-    expect(page).to have_content(lift_4.name)
-    expect(page).to have_content(lift_4.seats)
-    expect(page).to have_content("Is Gondola: #{lift_4.is_gondola}")
-    expect(page).to have_content("Lift Id: #{lift_4.id}")
-    expect(page).to have_content("Resort Id: #{lift_4.resort_id}")
+    expect(page).to have_content(@lift_4.name)
+    expect(page).to have_content(@lift_4.seats)
+    expect(page).to have_content("Is Gondola: #{@lift_4.is_gondola}")
+    expect(page).to have_content("Lift Id: #{@lift_4.id}")
+    expect(page).to have_content("Resort Id: #{@lift_4.resort_id}")
   end
 
 end 
--- spec/features/resorts/index_spec.rb
+++ spec/features/resorts/index_spec.rb
@@ -2,6 +2,17 @@ require "rails_helper"
 
 RSpec.describe "resorts index page", type: :feature do
 
+  before (:each) do
+    @resort_1 = Resort.create!(name: "Winter Park",
+                              location: "Grand County, Colorado",
+                              peak_elevation: 12060,
+                              is_ikon_resort: true)
+    @resort_2 = Resort.create!(name: "Copper Mountain",
+                              location: "Summit County, Colorado",
+                              peak_elevation: 12313,
+                              is_ikon_resort: true)
+  end
+
   it "has link to Resort Index" do
     visit "/resorts"
     
@@ -29,22 +40,13 @@ RSpec.describe "resorts index page", type: :feature do
   end
 
   it "can see all resorts and created datetime" do
-    resort_1 = Resort.create!(name: "Winter Park",
-                              location: "Grand County, Colorado",
-                              peak_elevation: 12060,
-                              is_ikon_resort: true)
-    resort_2 = Resort.create!(name: "Copper Mountain",
-                              location: "Summit County, Colorado",
-                              peak_elevation: 12313,
-                              is_ikon_resort: true)
-
     visit "/resorts"
 
-    expect(page).to have_content(resort_1.name)
-    expect(page).to have_content(resort_1.created_at)
+    expect(page).to have_content(@resort_1.name)
+    expect(page).to have_content(@resort_1.created_at)
 
-    expect(page).to have_content(resort_2.name)
-    expect(page).to have_content(resort_2.created_at)
+    expect(page).to have_content(@resort_2.name)
+    expect(page).to have_content(@resort_2.created_at)
  
   end
 
